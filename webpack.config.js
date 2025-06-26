0bf9d210404ff80b993473cdee0d241456083d8d
chandankumarpro <98318810+chandankumarpro@users.noreply.github.com>
2025-06-26 10:20:04 +0530

    vercel

--- webpack.config.js
+++ webpack.config.js
@@ -2,53 +2,84 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const Dotenv = require("dotenv-webpack");
 
-module.exports = {
-  entry: "./src/index.tsx",
-  output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "bundle.[contenthash].js",
-    publicPath: "/",
-    clean: true,
-  },
-  mode: "development",
-  devtool: "source-map",
-  resolve: {
-    extensions: [".tsx", ".ts", ".js"],
-  },
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        use: "babel-loader",
-        exclude: /node_modules/,
-      },
-      {
-        test: /\.scss$/,
-        use: ["style-loader", "css-loader", "sass-loader"],
-      },
-      {
-        enforce: "pre",
-        test: /\.js$/,
-        loader: "source-map-loader",
-      },
-    ],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: "./public/index.html",
-    }),
-    new Dotenv({
-      path: "./.env",
-      systemvars: true,
-      safe: false,
-    }),
-  ],
-  devServer: {
-    static: {
-      directory: path.join(__dirname, "dist"),
+module.exports = (env, argv) => {
+  const isProduction = argv.mode === "production";
+
+  return {
+    entry: "./src/index.tsx",
+
+    output: {
+      path: path.resolve(__dirname, "dist"),
+      filename: isProduction ? "bundle.[contenthash].js" : "bundle.js",
+      publicPath: "/",
+      clean: true,
     },
-    historyApiFallback: true,
-    port: 3000,
-    open: true,
-  },
+
+    mode: isProduction ? "production" : "development",
+
+    devtool: isProduction ? "hidden-source-map" : "source-map",
+
+    resolve: {
+      extensions: [".tsx", ".ts", ".js"],
+    },
+
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          use: "ts-loader",
+          exclude: /node_modules/,
+        },
+        {
+          test: /\.scss$/,
+          use: ["style-loader", "css-loader", "sass-loader"],
+        },
+        {
+          enforce: "pre",
+          test: /\.js$/,
+          loader: "source-map-loader",
+        },
+      ],
+    },
+
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: "./public/index.html",
+        minify: isProduction
+          ? {
+              collapseWhitespace: true,
+              removeComments: true,
+              removeRedundantAttributes: true,
+              useShortDoctype: true,
+              removeEmptyAttributes: true,
+              removeStyleLinkTypeAttributes: true,
+              keepClosingSlash: true,
+              minifyJS: true,
+              minifyCSS: true,
+              minifyURLs: true,
+            }
+          : false,
+      }),
+      new Dotenv({
+        path: "./.env",
+        systemvars: true,
+      }),
+    ],
+
+    devServer: {
+      static: {
+        directory: path.join(__dirname, "dist"),
+      },
+      historyApiFallback: true,
+      port: 3000,
+      open: true,
+    },
+
+    optimization: {
+      splitChunks: {
+        chunks: "all",
+      },
+      runtimeChunk: "single",
+    },
+  };
 };
